a92b960b06dded97bf96f2bb010563901054268f
Tabib243 <tabibw243@gmail.com>
2025-03-02 19:33:41 +0600

    trying to bug solving

--- Java-Script/main.js
+++ Java-Script/main.js
@@ -12,7 +12,7 @@ updateDate();
 
 // Redirect to secondery.html page
 document.getElementById('redirectBTN').addEventListener('click',function(){
-    window.location.href='./secondery-index.html';
+    window.location.href='../HTML/secondery.html';
 })
 
 // Theme changing button
