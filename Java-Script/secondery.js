2d583a84e870f16d732245b09bf990b059fc2c61
Tabib243 <tabibw243@gmail.com>
2025-03-02 21:45:20 +0600

    trying to bug solving

--- Java-Script/secondery.js
+++ Java-Script/secondery.js
@@ -1,4 +1,4 @@
 // Back to main.html page
 document.getElementById('goToHomePage').addEventListener('click', function() {
-    window.location.href = '../HTML/index.html';
+    window.location.href = '../index.html';
 });
